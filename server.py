f3e43ccf0cda26aba53b1b65e25b0289ee263fcc
Anton Kharhonov <vachovski4@gmail.com>
2020-05-14 19:02:09 +0300

    Update server.py

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ import connexion
 app = connexion.App(__name__, specification_dir='./')
 # Read the swagger.yml file to configure the endpoints
 app.add_api('swagger.yml')
-
+CORS(app.app)
 
 # Create a URL route in our application for "/"
 @app.route('/')
