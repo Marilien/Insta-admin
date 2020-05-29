06444ad239798933a51f3de3580e1466f468ef6f
tonskey <vachovski4@gmail.com>
2020-05-29 19:25:26 +0300

    make two separate routes

--- server.py
+++ server.py
@@ -22,8 +22,12 @@ def home():
 def get_account_info(username):
     return account_info(username)
 
-    
 @app.route('/api/followers/<user_id>')
+@cross_origin()
+def get_followers(user_id):
+    return followers(user_id)
+    
+@app.route('/api/2/followers/<user_id>')
 @cross_origin()
 def get_followers(user_id):
     from followers_service import followers
