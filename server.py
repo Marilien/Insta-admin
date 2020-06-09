5dd83c9980db1ee08e5b0cb22a222f2a4990f03b
Anton Kharhonov <vachovski4@gmail.com>
2020-06-10 00:29:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,59 +1,49 @@
 from flask import (Flask,
     render_template
 )
-from flask import jsonify
-from flask_socketio import SocketIO, send, emit
 from flask_cors import cross_origin, CORS
-# from rq import Queue
-# from rq.job import Job
-# from worker import conn
-
 
 from followers_service import account_info, followers, get_multiple_list_followers, get_accounts_info
 
-# q = Queue(connection=conn)
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
 @app.route('/')
-@cross_origin()
 def home():
     return render_template('home.html')
 
 @app.route('/api/account_info/<username>')
-@cross_origin()
 def get_account_info(username):
     return account_info(username)
 
 @app.route('/api/followers/<user_id>')
-@cross_origin()
 def get_followers(user_id):
     return followers(user_id)
 
-@socketio.on('followers')
-def handle_followers(user_id):
-    # print('answer from backend')
-    # emit('followers_response', user_id)
-    followers(user_id)
-
-
-@socketio.on('get_multiple_list_followers')
-def handle_multiple_followers(list_of_usedids):
-    # print('answer from backend')
-    # emit('followers_response', user_id)
-    get_multiple_list_followers(list_of_usedids)
-
-
-# @socketio.on('get_accounts_info')
-# def handle_accounts_info(list_of_usernames):
-#     # print('answer from backend')
-#     # emit('followers_response', user_id)
-#     get_accounts_info(list_of_usernames)
-
 @app.route('/api/get_accounts_info/<list_of_usernames>')
-@cross_origin()
 def multiple_accounts_info(list_of_usernames):
     return get_accounts_info(list_of_usernames)
+
+
+
+# @socketio.on('followers')
+# def handle_followers(user_id):
+#     # print('answer from backend')
+#     # emit('followers_response', user_id)
+#     followers(user_id)
+
+
+# @socketio.on('get_multiple_list_followers')
+# def handle_multiple_followers(list_of_usedids):
+#     # print('answer from backend')
+#     # emit('followers_response', user_id)
+#     get_multiple_list_followers(list_of_usedids)
+
+
+# # @socketio.on('get_accounts_info')
+# # def handle_accounts_info(list_of_usernames):
+# #     # print('answer from backend')
+# #     # emit('followers_response', user_id)
+# #     get_accounts_info(list_of_usernames)
 
 
 
@@ -89,6 +79,6 @@ def multiple_accounts_info(list_of_usernames):
 #         return "Nay!", 202
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
 ## marta.khoma 1424071908
 ## mongolo4ka_ 9264496810
