06d20e0483021bc34b3f60bbcb712ad75d1e5bec
Marilien <marilien.mo@gmail.com>
2020-06-10 19:54:07 +0300

    add new accounts

--- constants_local.py
+++ constants_local.py
@@ -32,7 +32,7 @@ LOGIN_DATA = [
     {"MY_USERNAME": "accounttest1868", "MY_PASSWORD": "ac1868"},
     {"MY_USERNAME": "project6805", "MY_PASSWORD": "pr6805"},
     # {"MY_USERNAME": "newaccount7547", "MY_PASSWORD": "np1234"},
-    {"MY_USERNAME": "instapr2020", "MY_PASSWORD": "inst2020"},
+    # {"MY_USERNAME": "instapr2020", "MY_PASSWORD": "inst2020"},
     {"MY_USERNAME": "instaadmin999", "MY_PASSWORD": "inst999"},
     {"MY_USERNAME": "tes_tproject", "MY_PASSWORD": "testproject"},
     {"MY_USERNAME": "masha24484", "MY_PASSWORD": "m24484"},
