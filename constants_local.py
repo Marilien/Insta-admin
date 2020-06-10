8faf6e379643b373042134a70ce26ac48ea77c53
Maria <marilien.mo@gmail.com>
2020-06-10 14:07:02 +0300

    add login account

--- constants_local.py
+++ constants_local.py
@@ -26,11 +26,11 @@ LOGIN_DATA = [
     # {"MY_USERNAME": "faketestuser3", "MY_PASSWORD": "Ds3kkGzYLv2FJy4"},
     {"MY_USERNAME": "accounttest1868", "MY_PASSWORD": "ac1868"},
     {"MY_USERNAME": "project6805", "MY_PASSWORD": "pr6805"},
-    {"MY_USERNAME": "newaccount7547", "MY_PASSWORD": "np1234"}
-    # {"MY_USERNAME": "instapr2020", "MY_PASSWORD": "inst2020"},
-    # {"MY_USERNAME": "instaadmin999", "MY_PASSWORD": "inst999"},
-    # {"MY_USERNAME": "tes_tproject", "MY_PASSWORD": "testproject"},
-    # {"MY_USERNAME": "masha24484", "MY_PASSWORD": "m24484"},
+    {"MY_USERNAME": "newaccount7547", "MY_PASSWORD": "np1234"},
+    {"MY_USERNAME": "instapr2020", "MY_PASSWORD": "inst2020"},
+    {"MY_USERNAME": "instaadmin999", "MY_PASSWORD": "inst999"},
+    {"MY_USERNAME": "tes_tproject", "MY_PASSWORD": "testproject"},
+    {"MY_USERNAME": "masha24484", "MY_PASSWORD": "m24484"},
 ]
 
 
